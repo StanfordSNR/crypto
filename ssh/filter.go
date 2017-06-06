182a8be2f490f88d44c9cdd1fff78f0156c5c7b3
Henri S <sternhenri2@gmail.com>
2017-06-06 02:36:37 -0700

    need to work on persistent storage

--- ssh/filter.go
+++ ssh/filter.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	"strings"
 
-	"github.com/dimakogan/ssh/gossh/store"
+	"github.com/dimakogan/ssh/gossh/policy"
 )
 
 const (
@@ -18,64 +18,57 @@ const (
 
 type PromptUserFunc func(txt string) (string, error)
 
-type Policy struct {
-	ClientHostname string
-	ClientPort     uint32
-	ClientUsername string
-
-	User                string
+type Filter struct {
+	// kept to validate that promised command is made command (may be unecessary since we don't check thereafter if all approved)
 	Command             string
-	Server              string
-	ApprovedAllCommands bool
+	Scope 				policy.Scope
 	SessionOpened       bool
 	NMSStatus           int
 	Prompt              PromptUserFunc
 }
 
-func (pc *Policy) GetKey() store.RequestedPerm {
-	return store.RequestedPerm{AUser: pc.User, AServer: pc.Server}
+func (fil *Filter) Approved() bool {
+	return fil.Scope.Approved(fil.Command)
 }
 
-func (pc *Policy) AskForApproval(scopedStore store.ScopedStore) error {
-	text := "."
-	var err error
-	// switch to regex
-	for err == nil && text != "y" && text != "n" && text != "a" && text != "" {
-		// if with wrapper, approval can be done only for session?
-		text, err = pc.Prompt(fmt.Sprintf("Approve %s@%s:%d running '%s' on %s@%s? Approve all future commands? [Y/n/a]:",
-			pc.ClientUsername, pc.ClientHostname, pc.ClientPort, pc.Command, pc.User, pc.Server))
-		text = strings.ToLower(strings.Trim(text, " \r\n"))
-	}
+func (fil *Filter) AskForApproval() error {
+    text := "."
+    var err error
+    // switch to regex
+    for err == nil && text != "y" && text != "n" && text != "a" && text != "" {
+        // if with wrapper, approval can be done only for session?
+        text, err = fil.Prompt(fmt.Sprintf("Approve %s@%s:%d running '%s' on %s@%s? Approve all future commands? [Y/n/a]:",
+            fil.Scope.ClientUsername, fil.Scope.ClientHostname, fil.Scope.ClientPort, fil.Command, fil.Scope.ServiceUsername, fil.Scope.ServiceHostname))
+        text = strings.ToLower(strings.Trim(text, " \r\n"))
+    }
 
-	if err != nil {
-		return err
-	}
-	if text == "n" {
-		err = errors.New("Policy rejected client request")
-	}
-	if text == "a" {
-		pc.ApprovedAllCommands = true
-		rules, exists := scopedStore.PolicyScope[pc.GetKey()]
-		if exists == false {
-			rules = *new(store.PolicyRule)
-		}
-		rules.AllCommands = true
-		scopedStore.PolicyScope[pc.GetKey()] = rules
-		err2 := scopedStore.Save()
-		if err2 != nil {
-			log.Printf("Saving policy to disk failed: %s", err2)
-		}
-		return err
-	}
-	return err
+    if err != nil {
+        return err
+    }
+    if text == "n" {
+        err = errors.New("Policy rejected client request")
+    }
+    if text == "a" {
+
+        rule := fil.Scope.GetRule()
+        rule.AllCommands = true
+        err := fil.Scope.SetRule(rule)
+
+        if err != nil {
+            log.Printf("Saving policy to disk failed: %s", err)
+        }
+        return err
+    }
+    // add a "y" check if you want to store one time approval.
+    return err
 }
 
-func (pc *Policy) EscalateApproval() error {
+func (fil *Filter) EscalateApproval() error {
 	var text string
 	var err error
 	// switch to regex
 	for err == nil && text != "y" && text != "n" {
-		text, err = pc.Prompt(fmt.Sprintf(`Allow  %s@%s:%d full control of %s@%s? [Y/n]:`, pc.ClientUsername, pc.ClientHostname, pc.ClientPort, pc.User, pc.Server))
+		text, err = fil.Prompt(fmt.Sprintf(`Allow  %s@%s:%d full control of %s@%s? [Y/n]:`, fil.Scope.ClientUsername, fil.Scope.ClientHostname, fil.Scope.ClientPort, fil.Scope.ServiceUsername, fil.Scope.ServiceHostname))
 		text = strings.ToLower(strings.Trim(text, " \r\n"))
 	}
 	if err != nil {
@@ -90,8 +83,8 @@ func (pc *Policy) EscalateApproval() error {
 	return err
 }
 
-func (pc *Policy) FilterServerPacket(packet []byte) (validState bool, response []byte, err error) {
-	if pc.NMSStatus != AwaitingReply {
+func (fil *Filter) FilterServerPacket(packet []byte) (validState bool, response []byte, err error) {
+	if fil.NMSStatus != AwaitingReply {
 		return true, nil, nil
 	}
 
@@ -100,17 +93,17 @@ func (pc *Policy) FilterServerPacket(packet []byte) (validState bool, response [
 		if debugProxy {
 			log.Printf("Server approved no-more-sessions.")
 		}
-		pc.NMSStatus = Success
+		fil.NMSStatus = Success
 	case msgRequestFailure:
 		if debugProxy {
 			log.Printf("Server sent no-more-sessions failure.")
 		}
-		pc.NMSStatus = Failure
+		fil.NMSStatus = Failure
 	}
 	return true, nil, nil
 }
 
-func (pc *Policy) FilterClientPacket(packet []byte) (allowed bool, response []byte, err error) {
+func (fil *Filter) FilterClientPacket(packet []byte) (allowed bool, response []byte, err error) {
 	decoded, err := decode(packet)
 	if err != nil {
 		return false, nil, err
@@ -118,10 +111,10 @@ func (pc *Policy) FilterClientPacket(packet []byte) (allowed bool, response []by
 
 	switch msg := decoded.(type) {
 	case *channelOpenMsg:
-		if msg.ChanType != "session" || pc.SessionOpened {
+		if msg.ChanType != "session" || fil.SessionOpened {
 			return false, Marshal(channelOpenFailureMsg{}), nil
 		} else {
-			pc.SessionOpened = true
+			fil.SessionOpened = true
 		}
 		return true, nil, nil
 	case *globalRequestMsg:
@@ -131,7 +124,7 @@ func (pc *Policy) FilterClientPacket(packet []byte) (allowed bool, response []by
 			if debugProxy {
 				log.Printf("Client sent no-more-sessions")
 			}
-			pc.NMSStatus = AwaitingReply
+			fil.NMSStatus = AwaitingReply
 		}
 		return true, nil, nil
 	case *channelRequestMsg:
@@ -144,15 +137,15 @@ func (pc *Policy) FilterClientPacket(packet []byte) (allowed bool, response []by
 		if err := Unmarshal(msg.RequestSpecificData, &execReq); err != nil {
 			return false, nil, err
 		}
-		if execReq.Command != pc.Command {
-			log.Printf("Unexpected command: %s, (expecting: %s)", execReq.Command, pc.Command)
+		if execReq.Command != fil.Command {
+			log.Printf("Unexpected command: %s, (expecting: %s)", execReq.Command, fil.Command)
 			return false, Marshal(channelRequestFailureMsg{}), nil
 		}
 		return true, nil, nil
 	case *kexInitMsg:
-		if pc.NMSStatus != Success && !pc.ApprovedAllCommands {
+		if fil.NMSStatus != Success && !fil.Scope.GetRule().AllCommands {
 			log.Printf("Requested kexInit without first sending no more sessions.")
-			if err = pc.EscalateApproval(); err != nil {
+			if err = fil.EscalateApproval(); err != nil {
 				return false, Marshal(disconnectMsg{Reason: 2, Message: "Must issue no-more-sessions before handoff"}), err
 			}
 		}
