bb857b1ee6d853b831ccae363c44fd04e634ba11
Dima Kogan <dimakogan@gmail.com>
2017-06-07 11:52:55 -0700

    Clean up

--- ssh/filter.go
+++ ssh/filter.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+
 	"github.com/dimakogan/ssh/gossh/policy"
 	i "github.com/sternhenri/interact"
 )
@@ -19,86 +20,83 @@ type PromptUserFunc func(txt string) (string, error)
 
 type Filter struct {
 	// kept to validate that promised command is made command (may be unecessary since we don't check thereafter if all approved)
-	Command             string
-	Store 				policy.Store
-	Scope 				policy.Scope
-	SessionOpened       bool
-	NMSStatus           int
-	Prompt              PromptUserFunc
+	Command       string
+	Store         policy.Store
+	Scope         policy.Scope
+	SessionOpened bool
+	NMSStatus     int
+	Prompt        PromptUserFunc
 }
 
 func NewFilter(givenScope policy.Scope, givenStore policy.Store, givenCommand string, givenPrompt PromptUserFunc) *Filter {
-	return &Filter {
-		Command:	givenCommand,
-	   	Store:	 	givenStore,
-		Scope:	 	givenScope,
-		Prompt:	 	givenPrompt,
+	return &Filter{
+		Command: givenCommand,
+		Store:   givenStore,
+		Scope:   givenScope,
+		Prompt:  givenPrompt,
 	}
 }
 
 func (fil *Filter) IsApproved() error {
-    storedRule, ok := fil.Store[fil.Scope]
-    if ok && storedRule.IsApproved(fil.Command) {
-    	return nil
-    }
-    return fil.askForApproval()
+	storedRule, ok := fil.Store[fil.Scope]
+	if ok && storedRule.IsApproved(fil.Command) {
+		return nil
+	}
+	return fil.askForApproval()
 }
 
 func (fil *Filter) askForApproval() error {
-    var resp int64
-    var err error
+	var resp int64
+	var err error
 
 	i.Run(&i.Interact{
 		Questions: []*i.Question{
 			{
 				Quest: i.Quest{
-                    Msg: fmt.Sprintf("Allow %s@%s:%d to run '%s' on %s@%s?", 
-                    	fil.Scope.ClientUsername, fil.Scope.ClientHostname, 
-                    	fil.Scope.ClientPort, fil.Command, fil.Scope.ServiceUsername, 
-                    	fil.Scope.ServiceHostname),
-                    Choices: i.Choices{
-                        Alternatives: []i.Choice{
-                            {
-                                Text: "Disallow",
-                            },
-                            {
-                                Text: "Allow once",
-                            },
-                            {
-                                Text: "Allow forever",
-                            },
-                            {
-                       		 	Text: fmt.Sprintf("Allow %s@%s:%d to run any command on %s@%s forever",
-                       		 		fil.Scope.ClientUsername, fil.Scope.ClientHostname,
-                       		 		fil.Scope.ClientPort, fil.Scope.ServiceUsername,
-                       		 		fil.Scope.ServiceHostname),
-                            },
-                        },
-                    },
-                },
-                Action: func(c i.Context) interface{} {
-                	fmt.Println("1 %s", c)
-                	fmt.Println("2 %s", c.Ans())
-                	// fmt.Println("3 %s", c.Ans().Int())
-                    resp, _ = c.Ans().Int()
-                    return nil
-                },
+					Msg: fmt.Sprintf("Allow %s@%s:%d to run '%s' on %s@%s?",
+						fil.Scope.ClientUsername, fil.Scope.ClientHostname,
+						fil.Scope.ClientPort, fil.Command, fil.Scope.ServiceUsername,
+						fil.Scope.ServiceHostname),
+					Choices: i.Choices{
+						Alternatives: []i.Choice{
+							{
+								Text: "Disallow",
+							},
+							{
+								Text: "Allow once",
+							},
+							{
+								Text: "Allow forever",
+							},
+							{
+								Text: fmt.Sprintf("Allow %s@%s:%d to run any command on %s@%s forever",
+									fil.Scope.ClientUsername, fil.Scope.ClientHostname,
+									fil.Scope.ClientPort, fil.Scope.ServiceUsername,
+									fil.Scope.ServiceHostname),
+							},
+						},
+					},
+				},
+				Action: func(c i.Context) interface{} {
+					resp, _ = c.Ans().Int()
+					return nil
+				},
 			},
 		},
 	})
 
 	switch resp {
 	case 1:
-        err = errors.New("Policy rejected client request")
-    case 2:
-    	err = nil
-    case 3:
-    	err = fil.Store.SetCommandAllowedInScope(fil.Scope, fil.Command)
-    case 4:
-    	err = fil.Store.SetAllAllowedInScope(fil.Scope)
+		err = errors.New("Policy rejected client request")
+	case 2:
+		err = nil
+	case 3:
+		err = fil.Store.SetCommandAllowedInScope(fil.Scope, fil.Command)
+	case 4:
+		err = fil.Store.SetAllAllowedInScope(fil.Scope)
 	}
 
-    return err
+	return err
 }
 
 func (fil *Filter) EscalateApproval() error {
@@ -109,28 +107,28 @@ func (fil *Filter) EscalateApproval() error {
 		Questions: []*i.Question{
 			{
 				Quest: i.Quest{
-                    Msg: fmt.Sprintf("Can't enforce permission for a single command. Allow %s@%s:%d to run any command on %s@%s?", 
-                    	fil.Scope.ClientUsername, fil.Scope.ClientHostname, 
-                    	fil.Scope.ClientPort, fil.Scope.ServiceUsername, 
-                    	fil.Scope.ServiceHostname),
-                    Choices: i.Choices{
-                        Alternatives: []i.Choice{
-                            {
-                                Text: "Disallow",
-                            },
-                            {
-                                Text: "Allow for session",
-                            },
-                            {
-                                Text: "Allow forever",
-                            },
-                        },
-                    },
-                },
-                Action: func(c i.Context) interface{} {
-                    resp, _ = c.Ans().Int()
-                    return nil
-                },
+					Msg: fmt.Sprintf("Can't enforce permission for a single command. Allow %s@%s:%d to run any command on %s@%s?",
+						fil.Scope.ClientUsername, fil.Scope.ClientHostname,
+						fil.Scope.ClientPort, fil.Scope.ServiceUsername,
+						fil.Scope.ServiceHostname),
+					Choices: i.Choices{
+						Alternatives: []i.Choice{
+							{
+								Text: "Disallow",
+							},
+							{
+								Text: "Allow for session",
+							},
+							{
+								Text: "Allow forever",
+							},
+						},
+					},
+				},
+				Action: func(c i.Context) interface{} {
+					resp, _ = c.Ans().Int()
+					return nil
+				},
 			},
 		},
 	})
@@ -141,7 +139,7 @@ func (fil *Filter) EscalateApproval() error {
 	case 2:
 		err = nil
 	case 3:
-    	err = fil.Store.SetAllAllowedInScope(fil.Scope)
+		err = fil.Store.SetAllAllowedInScope(fil.Scope)
 	}
 
 	return err
@@ -157,7 +155,7 @@ func (fil *Filter) FilterServerPacket(packet []byte) (validState bool, response 
 		if debugProxy {
 			log.Printf("Server approved no-more-sessions.")
 		}
-		fil.NMSStatus = Success 
+		fil.NMSStatus = Success
 	case msgRequestFailure:
 		if debugProxy {
 			log.Printf("Server sent no-more-sessions failure.")
@@ -177,19 +175,17 @@ func (fil *Filter) FilterClientPacket(packet []byte) (allowed bool, response []b
 	case *channelOpenMsg:
 		if msg.ChanType != "session" || fil.SessionOpened {
 			return false, Marshal(channelOpenFailureMsg{}), nil
-		} else {
-			fil.SessionOpened = true
 		}
+		fil.SessionOpened = true
 		return true, nil, nil
 	case *globalRequestMsg:
 		if msg.Type != NoMoreSessionRequestName {
 			return false, Marshal(globalRequestFailureMsg{}), nil
-		} else {
-			if debugProxy {
-				log.Printf("Client sent no-more-sessions")
-			}
-			fil.NMSStatus = AwaitingReply
 		}
+		if debugProxy {
+			log.Printf("Client sent no-more-sessions")
+		}
+		fil.NMSStatus = AwaitingReply
 		return true, nil, nil
 	case *channelRequestMsg:
 		if msg.Request != "exec" {
@@ -208,7 +204,7 @@ func (fil *Filter) FilterClientPacket(packet []byte) (allowed bool, response []b
 		return true, nil, nil
 	case *kexInitMsg:
 		if fil.NMSStatus != Success && !fil.Store.GetRule(fil.Scope).AllCommands {
-			log.Printf("Requested kexInit without first sending no more sessions.")
+			log.Printf("Attempting handoff without successful no-more-sessions.")
 			if err = fil.EscalateApproval(); err != nil {
 				return false, Marshal(disconnectMsg{Reason: 2, Message: "Must issue no-more-sessions before handoff"}), err
 			}
